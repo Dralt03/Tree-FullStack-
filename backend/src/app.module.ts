d3d371d8c510d86730405b84ed1480e24cf6e181
Sauren Sharma <sharmasaurenb@gmail.com>
2024-06-30 17:56:08 +0530

    First Commit

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { configDotenv } from 'dotenv';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+configDotenv();
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI!)],
   controllers: [AppController],
   providers: [AppService],
 })
